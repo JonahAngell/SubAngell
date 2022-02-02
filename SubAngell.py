028b1e758d2322cf8f9930cc7d9219f1fbf1c9d4
JonahAngell <98866775+JonahAngell@users.noreply.github.com>
2022-02-02 07:02:40 -0700

    Update SubAngell.py

--- SubAngell.py
+++ SubAngell.py
@@ -1,15 +1,18 @@
-SUBTRACK = "This is the sentence I will use as a test"
-BANLIST = {"sentence":"phrase", "test":"practice"}
+SUBTRACK = "This is the sentence I will use as a test. \n\nPlease do not touch"
+BANLIST = {"sentence":"phrase", "test":"practice", "please":"por favor"}
 
-subtitle_track = SUBTRACK.split()
-subtrack = '' 
-x = - 1
+subtitle_track = SUBTRACK.split("\n")
+subtrack = ''
 
-for word in subtitle_track:
+x = -1
+for line in subtitle_track:
     x += 1
-    if word in BANLIST:
-        subtrack = subtrack + BANLIST[word] + ' '
-    else:
-        subtrack = subtrack + word
+    subtitle_track[x] = subtitle_track[x].split()
+    for word in subtitle_track[x]:
+        if word.lower() in BANLIST:
+            subtrack = subtrack + BANLIST[word] + ' '
+        else:
+            subtrack = subtrack + word + ' '
+    subtrack = subtrack + '\n' 
 
 print(subtrack)

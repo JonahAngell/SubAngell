4b9803c142b97823744fde5577bb9e874d489644
JonahAngell <98866775+JonahAngell@users.noreply.github.com>
2022-02-01 22:21:51 -0700

    Update SubAngell.py

--- SubAngell.py
+++ SubAngell.py
@@ -2,6 +2,14 @@ SUBTRACK = "This is the sentence I will use as a test"
 BANLIST = {"sentence":"phrase", "test":"practice"}
 
 subtitle_track = SUBTRACK.split()
+subtrack = '' 
+x = - 1
 
 for word in subtitle_track:
-    
+    x += 1
+    if BANLIST(word):
+        subtrack = subtrack + BANLIST(word) + ' '
+    else
+        subtrack = subtrack + word
+
+print(subtrack)
